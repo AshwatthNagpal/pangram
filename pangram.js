ef3084602a19c3451b8f3eb6ca147a6f71587d51
Ashwatth Nagpal <ashwatthnagpal@Ashwatths-MacBook-Pro-2.local>
2019-01-17 00:21:44 +0530

    add code to evaluate test string with one missing alphabet

--- pangram.js
+++ pangram.js
@@ -1,5 +1,11 @@
 export const isPangram = (str) => {
-    if (str === '')
+    if (str == '')
         return false;
+    let allAlphabet = 'abcdefghijklmnopqrstuvwxyz';
+    for (let i = 0; i < allAlphabet.length; ++i) {
+        if (!str.includes(allAlphabet[i]))
+            return false;
+    }
+
     return true;
 }

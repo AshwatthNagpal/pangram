3bf6e4cf2204f162bc9de1f47a6bf92aa1b3d311
Ashwatth Nagpal <ashwatthnagpal@Ashwatths-MacBook-Pro-2.local>
2019-01-17 00:24:55 +0530

    add code to evaluate test string with mixed case

--- pangram.js
+++ pangram.js
@@ -1,6 +1,7 @@
 export const isPangram = (str) => {
     if (str == '')
         return false;
+    str=str.toLowerCase();
     let allAlphabet = 'abcdefghijklmnopqrstuvwxyz';
     for (let i = 0; i < allAlphabet.length; ++i) {
         if (!str.includes(allAlphabet[i]))
--- pangram.spec.js
+++ pangram.spec.js
@@ -21,19 +21,19 @@ describe('Pangram()', () => {
     expect(isPangram('five boxing wizards jump quickly at it')).toBe(false);
   });
 
-  xtest('pangram with underscores', () => {
+  test('pangram with underscores', () => {
     expect(isPangram('the_quick_brown_fox_jumps_over_the_lazy_dog')).toBe(true);
   });
 
-  xtest('pangram with numbers', () => {
+  test('pangram with numbers', () => {
     expect(isPangram('the 1 quick brown fox jumps over the 2 lazy dogs')).toBe(true);
   });
 
-  xtest('missing letters replaced by numbers', () => {
+  test('missing letters replaced by numbers', () => {
     expect(isPangram('7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog')).toBe(false);
   });
 
-  xtest('pangram with mixed case and punctuation', () => {
+  test('pangram with mixed case and punctuation', () => {
     expect(isPangram('"Five quacking Zephyrs jolt my wax bed."')).toBe(true);
   });
 

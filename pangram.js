700124cc1d076cc9d2835bde1782b62419ec9852
Ashwatth Nagpal <ashwatthnagpal@Ashwatths-MacBook-Pro-2.local>
2019-01-17 20:29:41 +0530

    rewrite entire code in declarative format

--- pangram.js
+++ pangram.js
@@ -1,12 +1,8 @@
-export const isPangram = (str) => {
-    if (str == '')
-        return false;
-    str=str.toLowerCase();
-    let allAlphabet = 'abcdefghijklmnopqrstuvwxyz';
-    for (let i = 0; i < allAlphabet.length; ++i) {
-        if (!str.includes(allAlphabet[i]))
-            return false;
-    }
-
-    return true;
-}
+export const isPangram = (sentence) => {
+  if (sentence === '') {
+    return false;
+  }
+  const lowercaseSentence = sentence.toLowerCase();
+  const allAlphabets = 'abcdefghijklmnopqrstuvwxyz';
+  return allAlphabets.split('').reduce((result, alphabet) => result && lowercaseSentence.includes(alphabet), true);
+};
